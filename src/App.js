f1197a4d842327ab4c87602fb556c455a34b9b95
Shawn_hash <sherlock01_zhu@outlook.com>
2023-06-08 14:29:15 +0800

    changed map and footer from App.js to MapPage.js

--- src/App.js
+++ src/App.js
@@ -7,39 +7,13 @@ import NotFound from "./pages/NotFound";
 import {AuthProvider, useAuth} from "./contexts/AuthContext";
 import Account from "./pages/Account";
 import PrivateRoute from "./pages/PrivateRoute";
-import Footer from "./components/Footer";
 import MapPage from "./pages/MapPage";
 
 
 function App() {
   const {currentUser} = useAuth();
 
-  {/* Footer Boilerplate */}
-  const markers = [
-      { id: 1, name: 'Marker 1', filter: 'Category A' },
-      { id: 2, name: 'Marker 2', filter: 'Category B' },
-      { id: 3, name: 'Marker 3', filter: 'Category A' },
-      { id: 4, name: 'Marker 4', filter: 'Category C' },
-  ];
-
-  const filterOptions = ['Category A', 'Category B', 'Category C'];
-
-  const handleFilterChange = (event) => {
-    const selectedFilter = event.target.value;
-    // Perform filtering logic based on the selected filter
-    // Update the map display accordingly
-    console.log('Selected filter:', selectedFilter);
-  };
-
-  const handleSearch = (searchText) => {
-    // Perform search logic based on the entered search text
-    // Update the map display accordingly
-    console.log('Search text:', searchText);
-  };
-  {/* Ends here */}
-
   return (
-    <div>
         <AuthProvider>
             <Routes>
                 <Route exact path="/" element={<Home />} />
@@ -54,17 +28,6 @@ function App() {
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </AuthProvider>
-
-        {/* Footer Boilerplate */}
-        <h1>Map Application</h1>
-        <Footer
-              markers={markers}
-              filterOptions={filterOptions}
-              onFilterChange={handleFilterChange}
-              onSearch={handleSearch}
-        />
-        {/* Ends here */}
-    </div>
   );
 }
 
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,11 +1,45 @@
 import {Link} from "react-router-dom";
 import Map from "../components/Map";
+import Footer from "../components/Footer";
 
 export default function MapPage() {
+  {/* Footer Boilerplate */}
+  const markers = [
+      { id: 1, name: 'Marker 1', filter: 'Category A' },
+      { id: 2, name: 'Marker 2', filter: 'Category B' },
+      { id: 3, name: 'Marker 3', filter: 'Category A' },
+      { id: 4, name: 'Marker 4', filter: 'Category C' },
+  ];
+
+  const filterOptions = ['Category A', 'Category B', 'Category C'];
+
+  const handleFilterChange = (event) => {
+    const selectedFilter = event.target.value;
+    // Perform filtering logic based on the selected filter
+    // Update the map display accordingly
+    console.log('Selected filter:', selectedFilter);
+  };
+
+  const handleSearch = (searchText) => {
+    // Perform search logic based on the entered search text
+    // Update the map display accordingly
+    console.log('Search text:', searchText);
+  };
+  {/* Ends here */}
+
     return (
         <div id="MapPage-whole">
             <h1>MapPage</h1>
             <Map />
+
+        {/* Footer Boilerplate */}
+        <Footer
+              markers={markers}
+              filterOptions={filterOptions}
+              onFilterChange={handleFilterChange}
+              onSearch={handleSearch}
+        />
+        {/* Ends here */}
         </div>
     );
 }
