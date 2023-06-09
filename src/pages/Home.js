ce1062700b8511b8606449180fd3cbba8ed0c604
Cheryl Y <opkisky@gmail.com>
2023-06-10 00:46:57 +0800

    add some random styling on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import './Home.css';
 import Sign from '../components/Sign'
+import bg from '../images/bg.jpg'
 import {Link} from "react-router-dom";
 export default function Home() {
     return (
         <div id="Home-whole">
-            <h1>Home</h1>
+            <div id='home'>
+                <img className='bg' src={bg}/>
+                <h1 className='logo'>A platform for gamers <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;to connect, communicate, and more...</h1>
+            </div>
             <Sign/>
         </div>
     );
