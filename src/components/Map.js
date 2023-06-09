5ccfe9577cefede3b07b7167f26fce085634283b
jhuang40 <jasperhuang@hotmail.com>
2023-06-08 18:08:24 -0700

    Fixed map taking up the entire screen

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import { Marker, Popup } from 'react-leaflet'
 
 
 function Map(props) {
-return <MapContainer center={[49.2677, -123.2420]} zoom={13} scrollWheelZoom={false}>
+return <MapContainer center={[49.2677, -123.2420]} zoom={13} scrollWheelZoom={false} className="MainMap">
 <TileLayer
   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
