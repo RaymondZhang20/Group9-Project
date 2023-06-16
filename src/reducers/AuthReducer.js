7e7a2abb2b42b069260182bb64a68b560f2c378b
Xueyong Zhang <eiczxy20@student.ubc.ca>
2023-06-16 01:02:10 -0700

    init reducer

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    loading_login_register: false,
+    loading_context: true,
+    error: "",
+    alert: false,
+    currentUser: {
+        email: "test@test.com",
+        id: "test",
+        accountName: "test_account",
+        location: ".....",
+        timeZone: "..."
+    }
+}
