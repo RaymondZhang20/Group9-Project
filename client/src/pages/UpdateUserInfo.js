b94c609b60dd3038a0ae57a1259e6ba9cdc254ab
Cheryl Y <opkisky@gmail.com>
2023-06-23 21:34:27 +0800

    display info on update page

--- client/src/pages/UpdateUserInfo.js
+++ client/src/pages/UpdateUserInfo.js
@@ -3,25 +3,20 @@ import {useAuth} from "../contexts/AuthContext";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import {useDispatch, useSelector} from "react-redux";
+import {getAccountAsync, updateAccountAsync} from "../redux/accountReducers/accountThunks";
 
 function UpdateUserInfo() {
-  const [userInfo, setUserInfo] = useState({});
   const { currentUser } = useAuth();
+  const dispatch = useDispatch();
+  const userInfo = useSelector(state => state.account.currentUser);
   const uid = currentUser.uid;
 
   useEffect(() => {
-    // Fetch the current user's information from the server
-    const fetchUserInfo = async () => {
-      const response = await fetch(`http://localhost:5000/users/${uid}`);
-      if (response.ok) {
-        const data = await response.json();
-        setUserInfo(data);
-      } else {
-        // Handle error
-      }
-    };
-    fetchUserInfo();
-  }, [uid]);
+    if (currentUser) {
+        dispatch(getAccountAsync(currentUser.uid));
+    }
+}, [currentUser]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -72,17 +67,17 @@ function UpdateUserInfo() {
             </Form.Text>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>First Name</Form.Label>
-              <Form.Control name="first-name" type="text" placeholder="Enter First Name" defaultValue={userInfo.firstName} />
+              <Form.Control name="first-name" type="text" placeholder="Enter First Name" defaultValue={userInfo.first_name} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control name="last-name" type="text" placeholder="Enter Last Name" defaultValue={userInfo.lastName} />
+              <Form.Control name="last-name" type="text" placeholder="Enter Last Name" defaultValue={userInfo.last_name} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicTimeZone">
               <Form.Label>Time Zone</Form.Label>
-              <Form.Control name = "time-zone" type="text" placeholder="Enter time zone" defaultValue={userInfo.timeZone} />
+              <Form.Control name = "time-zone" type="text" placeholder="Enter time zone" defaultValue={userInfo.time_zone} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicLocation">
@@ -105,40 +100,40 @@ function UpdateUserInfo() {
 
             <Form.Group className="mb-3" controlId="formBasicPlayingTime">
               <Form.Label>Prefer playing time</Form.Label>
-              <Form.Check name="playing-time" type="checkbox" label="Morning" value="Morning" />
-              <Form.Check name="playing-time" type="checkbox" label="Afternoon" value="Afternoon" />
-              <Form.Check name="playing-time" type="checkbox" label="Evening" value="Evening" />
+              <Form.Check name="playing-time" type="checkbox" label="Morning" value="Morning" defaultChecked={userInfo.play_time.includes("Morning")}/>
+              <Form.Check name="playing-time" type="checkbox" label="Afternoon" value="Afternoon" defaultChecked={userInfo.play_time.includes("Afternoon")}/>
+              <Form.Check name="playing-time" type="checkbox" label="Evening" value="Evening" defaultChecked={userInfo.play_time.includes("Evening")}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicLanguage">
               <Form.Label>Language spoken</Form.Label>
-                  <Form.Check name="language" type="checkbox" label="English" value="English" />
-                  <Form.Check name="language" type="checkbox" label="Spanish" value="Spanish" />
-                  <Form.Check name="language" type="checkbox" label="French" value="French" />
-                  <Form.Check name="language" type="checkbox" label="German" value="German" />
-                  <Form.Check name="language" type="checkbox" label="Mandarin" value="Mandarin" />
-                  <Form.Check name="language" type="checkbox" label="Cantonese" value="Cantonese" />
-                  <Form.Check name="language" type="checkbox" label="Japanese" value="Japanese" />
-                  <Form.Check name="language" type="checkbox" label="Korean" value="Korean"/>
-                  <Form.Check name="language" type="checkbox" label="Italian" value="Italian"/>
-                  <Form.Check name="language" type="checkbox" label="Portuguese" value="Portuguese"/>
-                  <Form.Check name="language" type="checkbox" label="Russian" value="Russian"/>
-                  <Form.Check name="language" type="checkbox" label="Arabic" value="Arabic"/>
-                  <Form.Check name="language" type="checkbox" label="Hindi" value="Hindi"/>
-                  <Form.Check name="language" type="checkbox" label="Bengali" value="Bengali"/>
-                  <Form.Check name="language" type="checkbox" label="Dutch" value="Dutch"/>
-                  <Form.Check name="language" type="checkbox" label="Swedish" value="Swedish"/>
-                  <Form.Check name="language" type="checkbox" label="Other" value="Other"/>
+                  <Form.Check name="language" type="checkbox" label="English" value="English" defaultChecked={userInfo.language.includes("English")}/>
+                  <Form.Check name="language" type="checkbox" label="Spanish" value="Spanish" defaultChecked={userInfo.language.includes("Spanish")}/>
+                  <Form.Check name="language" type="checkbox" label="French" value="French" defaultChecked={userInfo.language.includes("French")}/>
+                  <Form.Check name="language" type="checkbox" label="German" value="German" defaultChecked={userInfo.language.includes("German")}/>
+                  <Form.Check name="language" type="checkbox" label="Mandarin" value="Mandarin" defaultChecked={userInfo.language.includes("Mandarin")}/>
+                  <Form.Check name="language" type="checkbox" label="Cantonese" value="Cantonese" defaultChecked={userInfo.language.includes("Cantonese")}/>
+                  <Form.Check name="language" type="checkbox" label="Japanese" value="Japanese" defaultChecked={userInfo.language.includes("Japanese")}/>
+                  <Form.Check name="language" type="checkbox" label="Korean" value="Korean" defaultChecked={userInfo.language.includes("Korean")}/>
+                  <Form.Check name="language" type="checkbox" label="Italian" value="Italian" defaultChecked={userInfo.language.includes("Italian")}/>
+                  <Form.Check name="language" type="checkbox" label="Portuguese" value="Portuguese" defaultChecked={userInfo.language.includes("Portuguese")}/>
+                  <Form.Check name="language" type="checkbox" label="Russian" value="Russian" defaultChecked={userInfo.language.includes("Russian")}/>
+                  <Form.Check name="language" type="checkbox" label="Arabic" value="Arabic" defaultChecked={userInfo.language.includes("Arabic")}/>
+                  <Form.Check name="language" type="checkbox" label="Hindi" value="Hindi" defaultChecked={userInfo.language.includes("Hindi")}/>
+                  <Form.Check name="language" type="checkbox" label="Bengali" value="Bengali" defaultChecked={userInfo.language.includes("Bengali")}/>
+                  <Form.Check name="language" type="checkbox" label="Dutch" value="Dutch" defaultChecked={userInfo.language.includes("Dutch")}/>
+                  <Form.Check name="language" type="checkbox" label="Swedish" value="Swedish" defaultChecked={userInfo.language.includes("Swedish")}/>
+                  <Form.Check name="language" type="checkbox" label="Other" value="Other" defaultChecked={userInfo.language.includes("Other")}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPlatform">
               <Form.Label>Platform</Form.Label>
-              <Form.Check name="platform" type="checkbox" label="Phone" value="Phone"/>
-              <Form.Check name="platform" type="checkbox" label="PC" value="PC"/>
-              <Form.Check name="platform" type="checkbox" label="PS" value="PS"/>
-              <Form.Check name="platform" type="checkbox" label="XBOX" value="XBOX"/>
-              <Form.Check name="platform" type="checkbox" label="NS" value="NS"/>
-              <Form.Check name="platform" type="checkbox" label="Other" value="Other"/>
+              <Form.Check name="platform" type="checkbox" label="Phone" value="Phone" defaultChecked={userInfo.platform.includes("Phone")}/>
+              <Form.Check name="platform" type="checkbox" label="PC" value="PC" defaultChecked={userInfo.platform.includes("PC")}/>
+              <Form.Check name="platform" type="checkbox" label="PS" value="PS" defaultChecked={userInfo.platform.includes("PS")}/>
+              <Form.Check name="platform" type="checkbox" label="XBOX" value="XBOX" defaultChecked={userInfo.platform.includes("XBOX")}/>
+              <Form.Check name="platform" type="checkbox" label="NS" value="NS" defaultChecked={userInfo.platform.includes("NS")}/>
+              <Form.Check name="platform" type="checkbox" label="Other" value="Other" defaultChecked={userInfo.platform.includes("Other")}/>
             </Form.Group>
 
             <Button variant="primary" type="submit">
