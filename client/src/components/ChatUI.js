93f7db942443f58c6adfdd581c511f24a127be61
dorager91 <dorothy.unicorn@gmail.com>
2023-07-13 12:33:15 -0700

    data structure and loads user chat history on select

--- client/src/components/ChatUI.js
+++ client/src/components/ChatUI.js
@@ -2,27 +2,18 @@ import React from 'react';
 import './ChatUI.css';
 import ChatMessage from './ChatMessage';
 
-const ChatUI = () => {
-    // replace with actual data
-    const friend = { username: 'Dorothy111', avatar: '/path/to/avatar.jpg' };
-    const messages = [
-        { text: "Hello!", sentByMe: true },
-        { text: "Hi, how are you?", sentByMe: false },
-        { text: "I'm good, thanks. And you?", sentByMe: true },
-        { text: "I'm fine too. Thanks for asking!", sentByMe: false },
-    ];
-
+const ChatUI = ({ friend }) => {
     return (
         <div className="chat-ui">
             <div className="title">Messages</div>
             <div className="chat-header">
                 <img src={friend.avatar} alt={friend.username} />
                 <h4>{friend.username}</h4>
-                {/* replace with actual icon */}
+                {/* replace with icon? */}
                 <button>Voice Call</button>
             </div>
             <div className="chat-body">
-                {messages.map((message, index) => (
+                {friend.messages.map((message, index) => (
                     <ChatMessage message={message} key={index} />
                 ))}
             </div>
--- client/src/components/FriendList.js
+++ client/src/components/FriendList.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import FriendCard from './FriendCard';
 
-const FriendsList = ({ friends }) => {
+const FriendsList = ({ friends, onSelectFriend, selectedFriend }) => {
     return (
         <div>
             <div className="title">Friends</div>
             {friends.map(friend => (
-                <FriendCard friend={friend} key={friend.id} />
+                <FriendCard
+                    friend={friend}
+                    key={friend.id}
+                    onClick={() => onSelectFriend(friend.id)}
+                    isSelected={friend.id === selectedFriend.id}
+                />
             ))}
         </div>
     );
 };
+
 
 export default FriendsList;
