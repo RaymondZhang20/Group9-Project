2f7e59f30de334484625d813aec2b17d9de2761e
dorager91 <dorothy.unicorn@gmail.com>
2023-07-26 00:07:05 -0700

    adding timestamp under each message

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -3,9 +3,12 @@ import './ChatMessage.css';
 
 const ChatMessage = ({ message }) => {
     const align = message.sentByMe ? 'right' : 'left';
+    // Format the timestamp
+    const timestamp = new Date(message.timeStamp).toLocaleString();
     return (
         <div className={`chat-message ${align}`}>
             <p>{message.content}</p>
+            <span className="timestamp">{timestamp}</span>
         </div>
     );
 };
