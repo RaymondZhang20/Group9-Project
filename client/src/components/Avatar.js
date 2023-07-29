4554430c7d3b7c66a534d5fd8035093efa4169bc
jhuang40 <jasperhuang@hotmail.com>
2023-07-28 21:21:53 -0700

    Changed localhost to url

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card} from 'react-bootstrap'
 
 export default function Avatar({avatar, alt, className, style, onClick}) {
-    const url = `http://localhost:5000/img/avatar/${avatar}`;
+    const url = `https://room9-backend.onrender.com/img/avatar/${avatar}`;
     return (
         <Card.Img src={url} alt={alt} className={className} style={style} onClick={onClick}/>
     );
