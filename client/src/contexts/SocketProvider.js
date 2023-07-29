a4dcdf590464f636fcb8fd648effd9ca0b2eef04
jhuang40 <jasperhuang@hotmail.com>
2023-07-28 17:23:56 -0700

    Removed env port from bin www

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -13,7 +13,7 @@ export function SocketProvider({ id, children }) {
     useEffect(() => {
         if (id !== 0) {
             const newSocket = io(
-                'http://localhost:5001',
+                '',
                 {query: {id}}
             )
             setSocket(newSocket);
