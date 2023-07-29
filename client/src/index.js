62cc83e50ef387cfb18560dceea524db8f843d08
jhuang40 <jasperhuang@hotmail.com>
2023-07-29 14:35:25 -0700

    Fixed import

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from './redux/store'
 import { Provider } from 'react-redux';
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
